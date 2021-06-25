aaeeea00d327e63f5af22c81de9ce5c6e064e6c1
Uchidero Krauch <37419270+asceso@users.noreply.github.com>
2021-06-25 16:05:34 +0300

    Добавьте файлы проекта.

---
+++ PassportDateHelper/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PassportDateHelper
+{
+    class Program
+    {
+        private static int[] PassportAges = new int[] { 14, 20, 45 };
+
+        static void Main()
+        {
+            Console.WriteLine("Input date in format dd.mm.yyyy");
+            var input_date_string = Console.ReadLine();
+            DateTime.TryParse(input_date_string, out DateTime birthday);
+            int age = (int)((DateTime.Now - birthday).TotalDays / 365);
+            int passport_index = GetMinIndexForPassportAges(age);
+            if (passport_index == -1)
+            {
+                Console.WriteLine("No passport");
+            }
+            else
+            {
+                Console.WriteLine($"Birthday is : {birthday.ToShortDateString()}");
+                Console.WriteLine($"Age is : {age}");
+                Console.WriteLine($"Passport last age : {PassportAges[passport_index]}");
+
+                DateTime receive_passport_start = birthday.AddYears(PassportAges[passport_index]);
+                DateTime receive_passport_end = receive_passport_start.AddDays(14);
+
+                Console.WriteLine($"Passport received between {receive_passport_start.ToShortDateString()} - {receive_passport_end.ToShortDateString()}");
+            }
+            Console.ReadKey();
+        }
+        static int GetMinIndexForPassportAges(int age)
+        {
+            if (age >= PassportAges[0] && age < PassportAges[1]) return 0;
+            else if (age >= PassportAges[1] && age < PassportAges[2]) return 1;
+            else if (age >= PassportAges[2]) return 2;
+            return -1;
+        }
+    }
+}
---
+++ SnilsCNC/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnilsCNC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Считываем число
+
+            Console.WriteLine("Enter SNILS Number (9 nums with - separator)");
+            string snils = Console.ReadLine();
+            string[] parts = snils.Split('-');
+
+            #endregion
+            #region Создаем список из всех чисел
+
+            List<int> snils_nums = new List<int>();
+            foreach (var item in parts)
+            {
+                foreach (var ch in item)
+                {
+                    snils_nums.Add(int.Parse(ch.ToString()));
+                }
+            }
+
+            #endregion
+            #region Умножаем на позицию (обратный порядок)
+
+            List<int> snils_multiples = new List<int>();
+            int position = 1;
+            for (int i = snils_nums.Count - 1; i >= 0; i--)
+            {
+                snils_multiples.Add(snils_nums[i] * position++);
+            }
+
+            #endregion
+            #region Заводим локальную переменную для расчета и считаем сумму произведений
+
+            int snils_cnc = 0;
+            int sum = snils_multiples.Sum();
+
+            #endregion
+            #region Проводим сверку числа по правилам
+
+        checking:
+            if (sum < 100)
+            {
+                snils_cnc = sum;
+            }
+            else if (sum == 100 || sum == 101)
+            {
+                snils_cnc = 00;
+            }
+            else if (sum > 101)
+            {
+                sum -= 101;
+                goto checking;
+            }
+
+            #endregion
+            #region Выводим CNC. Profit!
+
+            Console.WriteLine("CNC is :" + snils_cnc);
+            Console.ReadKey(); 
+
+            #endregion
+        }
+    }
+}
